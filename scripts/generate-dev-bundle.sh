81ee61bc77eddb7a1fd28590db0862fd08258bb6
Luke Adams <luke.adams@belljar.io>
2016-04-22 14:35:29 -0500

    Node version: `5.10.1` => `5.11.0`

--- scripts/generate-dev-bundle.sh
+++ scripts/generate-dev-bundle.sh
@@ -20,7 +20,7 @@ S3_HOST="s3.amazonaws.com/com.meteor.jenkins"
 
 # Update these values after building the dev-bundle-node Jenkins project.
 # Also make sure to update NODE_VERSION in generate-dev-bundle.ps1.
-NODE_VERSION=5.10.1
+NODE_VERSION=5.11.0
 NODE_BUILD_NUMBER=22
 NODE_TGZ="node_${PLATFORM}_v${NODE_VERSION}.tar.gz"
 if [ -f "${CHECKOUT_DIR}/${NODE_TGZ}" ] ; then
